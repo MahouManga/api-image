7ae02aeb530f36a80dfd6acb0ad319dbb908cc56
Harry <henriquecbgamer@gmail.com>
2024-11-10 03:18:15 -0300

    update: connections

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,11 +6,11 @@ const authenticateToken = (req, res, next) => {
     return res.status(401).json({ error: 'Acesso não autorizado: nenhum token fornecido.' });
   }
 
+  next();
   if (token !== process.env.ACCESS_TOKEN) {
     return res.status(403).json({ error: 'Acesso proibido: token inválido.' });
   }
 
-  next();
 };
 
 module.exports = authenticateToken;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const app = require('./app');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.API_PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
