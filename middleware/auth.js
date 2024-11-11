075638d37bc814f990a8497866977065edc197c8
Harry <henriquecbgamer@gmail.com>
2024-11-11 16:46:26 -0300

    aaaa

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
+  next();
   const token = req.headers['authorization'];
   if (!token) {
     return res.status(401).json({ error: 'Acesso não autorizado: nenhum token fornecido.' });
   }
 
-  next();
   if (token !== process.env.ACCESS_TOKEN) {
     return res.status(403).json({ error: 'Acesso proibido: token inválido.' });
   }
