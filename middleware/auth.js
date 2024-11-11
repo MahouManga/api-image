dbdb940a7d66e68c181a64c80007db9c6af1c4cc
Harry <henriquecbgamer@gmail.com>
2024-11-11 18:05:32 -0300

    preciso testar isso urgente

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
-  next();
   const token = req.headers['authorization'];
   if (!token) {
     return res.status(401).json({ error: 'Acesso não autorizado: nenhum token fornecido.' });
   }
 
   if (token !== process.env.ACCESS_TOKEN) {
-    return res.status(403).json({ error: 'Acesso proibido: token inválido.' });
+    return res.status(403).json({ error: 'Acesso proibido: token inválido.', token: token + ' - ' + process.env.ACCESS_TOKEN });
   }
+  next();
 
 };
 
